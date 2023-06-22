4f7886b762ac58b586f4df3e6c774757e5acd0f8
Adeniyii <ifedayoadeniyi@gmail.com>
2023-06-22 14:11:35 +0100

    feat: resore password policy

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 -- setup MYSQL server for airbnb project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-SET GLOBAL validate_password.policy=LOW;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
