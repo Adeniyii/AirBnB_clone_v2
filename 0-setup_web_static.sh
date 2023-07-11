90a707866f9fcac8b7ea699f56db977fae39708b
Adeniyii <ifedayoadeniyi@gmail.com>
2023-07-11 22:14:57 +0100

    fix format

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,13 +2,13 @@
 # Script that sets up your web servers for the deployment of web_static
 
 # install nginx
-sudo apt update
-sudo apt install nginx -y
+sudo apt-get update >/dev/null
+sudo apt-get install nginx -y >/dev/null
 
 #setup source directories
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" >/data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
