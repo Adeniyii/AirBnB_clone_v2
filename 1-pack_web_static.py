4ab8bd461a5559d0130333727506baff03b33cf2
Adeniyii <ifedayoadeniyi@gmail.com>
2023-07-15 17:34:29 +0100

    fix datetime

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,11 +3,14 @@
 from the contents of the web_static folder"""
 
 from fabric.api import *
+from datetime import datetime
 
 
 def do_pack():
     """Generate an archive of /web_static folder"""
 
+    d = datetime.now()
+    date = d.strftime('%Y%m%d%H%M%S')
+
     local("mkdir versions -p")
-    date = local("date '+%Y%m%d%H%M%s'", capture=True)
     local("tar -czvf versions/web_static_{}.tgz web_static".format(date))
