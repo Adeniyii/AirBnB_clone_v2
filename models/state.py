b374e48d7cf56745cf99dfea028af7498738b0b5
Adeniyii <ifedayoadeniyi@gmail.com>
2023-07-22 01:37:34 +0100

    fix

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
     __tablename__ = 'states'
     name = Column('name', String(128), nullable=False)
