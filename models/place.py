d99d3b3e97f4df06c7248fc090823dd0719f9c55
Adeniyii <ifedayoadeniyi@gmail.com>
2023-07-24 22:45:25 +0100

    fix: change more nullable values

--- models/place.py
+++ models/place.py
@@ -13,15 +13,15 @@ class Place(BaseModel, Base):
     user_id = Column(
         'user_id', String(60), ForeignKey('users.id'), nullable=False)
     name = Column("name", String(128), nullable=False)
-    description = Column("description", String(1024), nullable=False)
+    description = Column("description", String(1024))
     number_rooms = Column("number_rooms", Integer(), nullable=False, default=0)
     number_bathrooms = Column(
         "number_bathrooms", Integer(), nullable=False, default=0)
     max_guest = Column("max_guest", Integer(), nullable=False, default=0)
     price_by_night = Column(
         "price_by_night", Integer(), nullable=False, default=0)
-    latitude = Column("latitude", Float(), nullable=False, default=0)
-    longitude = Column("longitude", Float(), nullable=False, default=0)
+    latitude = Column("latitude", Float(), default=0)
+    longitude = Column("longitude", Float(), default=0)
     reviews = relationship(
         "Review", backref="place", cascade="all, delete, delete-orphan")
     amenity_ids = []
