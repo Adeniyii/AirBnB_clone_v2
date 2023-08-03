eeb72355de7a9f5ffceda041bd297bdd5c6ae684
Adeniyii <ifedayoadeniyi@gmail.com>
2023-08-03 13:55:08 +0100

    feat: add better docs

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask entry file"""
+"""Starts a flask application, and defines a single route"""
 from flask import Flask
 
 app = Flask(__name__)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a flask application"""
+"""Starts a flask application, and defines 2 routes"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,11 +7,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_hbnb():
+    """handle requests for the root route"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb")
 def main_hbnb():
+    """handle requests for the hbnb route"""
     return "HBNB"
 
 
